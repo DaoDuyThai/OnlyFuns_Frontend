1887878b2ce02c86aea6f0a2e5b8272e936fa231
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-02-15 10:18:14 +0700

    basically finish navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,83 @@
+
+import { useState } from 'react';
+
 const Header = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed);
+    };
     return (
         <>
-            <h1>Header</h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <img
+                            width="50px"
+                            height="50px"
+                            className="img-circle"
+                            src="src\assets\logo\OnlyFunsShortIcon.png"
+                        />
+                    </a>
+                    <button className="navbar-toggler" type="button" onClick={toggleCollapse}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContent">
+                        <form className="d-flex input-group w-auto ms-lg-3 my-3 my-lg-0">
+                            <input type="search" className="form-control" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-primary" type="button" data-mdb-ripple-color="dark">
+                                <div>
+                                    <i className="fas fa-search fa-lg mb-1"></i>
+                                </div>
+                            </button>
+                        </form>
+                        <ul className="navbar-nav me-auto d-flex flex-row mt-3 mt-lg-0">
+                        </ul>
+                        <ul className="navbar-nav me-auto ms-auto d-flex flex-row mt-3 mt-lg-0">
+                            <li className="nav-item text-center mx-2 mx-lg-1">
+                                <img
+                                    height="50px"
+                                    className="rounded-circle"
+                                    src="src/assets/logo/OnlyFunsLongIcon.png"
+                                />
+                            </li>
+                        </ul>
+
+                        <ul className="navbar-nav ms-auto d-flex flex-row mt-3 mt-lg-0">
+                            <li className="nav-item text-center mx-2 mx-lg-1">
+                                <a className="nav-link" href="#!">
+                                    <div>
+                                        <i className="fas fa-message fa-lg mb-1"></i>
+                                        <span className="badge rounded-pill badge-notification bg-danger">11</span>
+                                    </div>
+                                    Messages
+                                </a>
+                            </li>
+                            <li className="nav-item text-center mx-2 mx-lg-1">
+                                <a className="nav-link" href="#!">
+                                    <div>
+                                        <i className="fas fa-bell fa-lg mb-1"></i>
+                                        <span className="badge rounded-pill badge-notification bg-danger">11</span>
+                                    </div>
+                                    Notifications
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#!">
+                                    <div>
+                                        <img
+                                            height="50px"
+                                            className="avatar-img rounded-circle"
+                                            src="https://scontent.fhan17-1.fna.fbcdn.net/v/t39.30808-6/318135943_1321200128668980_1721583373262765542_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=HXCk9brqwrAAX-UkCtN&_nc_ht=scontent.fhan17-1.fna&oh=00_AfDdX4hSgNzqIWBXBHW--Sc2-ufqoTn-bRv7Sfybmjl-cw&oe=65D3605B"
+                                        />
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav >
         </>
     );
 };
