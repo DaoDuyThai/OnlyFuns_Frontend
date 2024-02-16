d7f8033d997eee278563e4290a000e05cfd2c603
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-02-16 09:24:49 +0700

    Finish layout for homescreen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
+import "./Home.css";
 import Header from "../../components/Header/Header";
-import HomeContent from "../../components/HomeContent/HomeContent";
+import LeftSide from "../../components/LeftSide/LeftSide";
+import PostSide from "../../components/PostSide/PostSide";
+import RightSide from "../../components/RightSide/RightSide";
 const Home = () => {
     return (
         <>
             <Header />
-
-            <HomeContent />
+            <div className="Home">
+                <RightSide />
+                <PostSide />
+                <LeftSide />
+            </div>
         </>
     );
 };
