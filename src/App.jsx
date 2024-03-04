e3c765b1b0466552b4d2387845baeb6bc56fe48b
Phuockingboy2 <phuocvip2@gmail.com>
2024-03-04 09:18:26 +0700

    improve-architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Post from './pages/Post/Post';
 import DashBoard from './pages/Admin/DashBoard/DashBoard.jsx';
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import SearchPage from "./pages/Search/Search";
-import SearchPost from "./pages/Search/SearchPost";
-import SearchPeople from "./pages/Search/SearchPeople";
-import ForgotPassword from "./pages/auth/FogotPassword";
-import Verify from "./pages/auth/Verify";
-import LoadingPage from "./pages/auth/RedirectPage";
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import SearchPage from './pages/Search/Search';
+import SearchPost from './pages/Search/SearchPost';
+import SearchPeople from './pages/Search/SearchPeople';
+import ForgotPassword from './pages/auth/FogotPassword';
+import Verify from './pages/auth/Verify';
+import LoadingPage from './pages/auth/RedirectPage';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import Profile from './pages/Profile/Profile.jsx';
@@ -21,77 +20,89 @@ import Chat from './pages/Chat/Chat.jsx';
 
 import ListMember from './components/admin-listmember/ListMember.jsx';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
+    const [accessToken, setAccessToken] = useState(
+        localStorage.getItem('accessToken'),
+    );
 
-  useEffect(() => {
-    setAccessToken(localStorage.getItem('accessToken'));
-  }, []);
-  
-  return (
-    <>
-      <ToastContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
+    useEffect(() => {
+        setAccessToken(localStorage.getItem('accessToken'));
+    }, []);
 
-          <Route path="/register" element={<Register />} />
+    return (
+        <>
+            <ToastContainer />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
 
-          <Route path="/verify/*" element={<Verify />} />
+                    <Route path="/register" element={<Register />} />
 
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
+                    <Route path="/verify/*" element={<Verify />} />
 
-          <Route path="/redirectpage" element={<LoadingPage />} />
+                    <Route
+                        path="/forgotpassword"
+                        element={<ForgotPassword />}
+                    />
 
-          {/*
-          * @des home page of a social network
-          * @author Đào Duy Thái
-          * @date 14/02/2024
-          */}
-          <Route path="/" element={<Home />} />
+                    <Route path="/redirectpage" element={<LoadingPage />} />
 
+                    {/*
+                     * @des home page of a social network
+                     * @author Đào Duy Thái
+                     * @date 14/02/2024
+                     */}
+                    <Route path="/" element={<Home />} />
 
-          {/*
-          * @des view post details of a social network
-          * @author Đào Duy Thái
-          * @date 14/02/2024
-          */}
-          <Route path="/post" element={<Post />} />
+                    {/*
+                     * @des view post details of a social network
+                     * @author Đào Duy Thái
+                     * @date 14/02/2024
+                     */}
+                    <Route path="/post" element={<Post />} />
 
-          <Route path="/search/*" element={<SearchPage />} />
+                    <Route path="/search/*" element={<SearchPage />} />
 
-          <Route path="/search/people" element={<SearchPeople />} />
+                    <Route path="/search/people" element={<SearchPeople />} />
 
-          <Route path="/search/posts" element={<SearchPost />} />
+                    <Route path="/search/posts" element={<SearchPost />} />
 
-          <Route path="/profile" element={<Profile />} />
+                    <Route path="/profile" element={<Profile />} />
 
-          <Route path="/profile/edit" element={<EditProfile />} />
+                    <Route path="/profile/edit" element={<EditProfile />} />
 
-          <Route path="/connections" element={<Connections />} />
+                    <Route path="/connections" element={<Connections />} />
 
-          <Route path="/notifications" element={<Notifications />} />
+                    <Route path="/notifications" element={<Notifications />} />
 
-          <Route path="/chat" element={<Chat />} />
+                    <Route path="/chat" element={<Chat />} />
 
-          <Route path="/listreports" element={<h1>List Reports</h1>} />
+                    <Route
+                        path="/listreports"
+                        element={<h1>List Reports</h1>}
+                    />
 
-          <Route path="/dashboard" element={<DashBoard />} />
+                    <Route path="/dashboard" element={<DashBoard />} />
 
-          <Route path="/listmembers" element={<ListMember/>}  />
+                    <Route path="/listmembers" element={<ListMember />} />
 
-          <Route path="/listbanners" element={<h1>List Banners</h1>} />
+                    <Route
+                        path="/listbanners"
+                        element={<h1>List Banners</h1>}
+                    />
 
-          <Route path="/editbanners" element={<h1>Edit Banners</h1>} />
+                    <Route
+                        path="/editbanners"
+                        element={<h1>Edit Banners</h1>}
+                    />
 
-          <Route path="*" element={<h1>Not Found</h1>} />
-
-        </Routes>
-      </ BrowserRouter>
-    </>
-  )
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,5 +1,14 @@
 import axios from '../setup/axios.js';
 
+/**
+ * Fetches user activity data for the last 7 days from a specified path.
+ *
+ * @async
+ * @author PhuocDT
+ * @param {string} path - The path where the user activity data is located.
+ * @returns {Promise<Object>} The user activity data.
+ * @throws {Error} When unable to fetch the data.
+ */
 async function getUserLast7Days(path) {
     try {
         const data = await axios.get(path);
@@ -9,6 +18,15 @@ async function getUserLast7Days(path) {
     }
 }
 
+/**
+ * Fetches user data from a specified path.
+ *
+ * @async
+ * @author PhuocDT
+ * @param {string} path - The path where the user data is located.
+ * @returns {Promise<Object>} The user data.
+ * @throws {Error} When unable to fetch the data.
+ */
 async function getUsers(path) {
     try {
         const response = await axios.get(path);
@@ -18,6 +36,15 @@ async function getUsers(path) {
     }
 }
 
+/**
+ * Fetches data for a specific user by their ID.
+ *
+ * @async
+ * @author PhuocDT
+ * @param {string} id - The ID of the user.
+ * @returns {Promise<Object>} The user data.
+ * @throws {Error} When unable to fetch the data.
+ */
 async function getUser(id) {
     try {
         const response = await axios.get(`/user/${id}`);
