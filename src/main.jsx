a9a1a4c09ab68dd32043fb91c0b29d21382333a3
phucne123111 <134253765+phucne123111@users.noreply.github.com>
2024-03-04 13:23:15 +0700

    userProfile
    userProfile

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,20 @@ import App from './App.jsx';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Verify from './pages/auth/Verify.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        {/* <BrowserRouter>
+            <Routes>
+                <Route path="/verify/:verificationCode" element={<Verify />} />
+            </Routes>
+        </BrowserRouter> */}
+
+
+    </>
 );
